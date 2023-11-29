2dbdce1c2df6c012c122d008e3b83c5d3d712e14
Ritesh <codeshorts007@gmail.com>
2023-11-29 17:35:43 +0530

    style- responsive navbar

---
+++ components/Header.js
@@ -0,0 +1,61 @@
+"use client";
+import Image from "next/image";
+import { FaBars } from "react-icons/fa6";
+import Mobile from "./Mobile";
+import { useState } from "react";
+import Link from "next/link";
+
+const Header = () => {
+  const [show, setShow] = useState(false);
+  return (
+    <div className="w-full h-20 p-1  flex items-center justify-between relative">
+      <div className="w-1/3 flex items-center justify-start px-2">
+        <Link href="/">
+          <Image src="/logo.png" alt="logo" width={50} height={50} />
+        </Link>
+      </div>
+      <div className="w-1/3 hidden md:flex items-center justify-center">
+        <ul>
+          <li className="inline-block px-2 text-base font-semibold">
+            <Link href="/">Home</Link>
+          </li>
+          <li className="inline-block px-2 text-base font-semibold">
+            <Link href="/about">Blog</Link>
+          </li>
+          <li className="inline-block px-2 text-base font-semibold">
+            <Link href="/contact">About</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="w-1/3 hidden md:flex items-center justify-end px-2">
+        <ul>
+          <li className="inline-block px-2 text-sm font-semibold mx-2">
+            <Link href="/login">Login</Link>
+          </li>
+          <li className="inline-block bg-black text-white p-2 rounded-md shadow text-sm font-semibold">
+            <Link href="/register">Sign up</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="w-1/3 md:hidden flex items-center justify-end px-2">
+        <span
+          className="text-white bg-gray-800 p-1 rounded-sm shadow cursor-pointer"
+          onClick={() => setShow(!show)}
+        >
+          <FaBars fontSize={17} />
+        </span>
+        {show ? (
+          <span className="fixed w-full h-full bg-gradient-to-t from-gray-700 via-gray-900 to-black left-0 top-0 transition-transform ">
+            <Mobile setShow={setShow} />
+          </span>
+        ) : (
+          <span className="fixed w-full h-full bg-gradient-to-t from-gray-700 via-gray-900 to-black left-0 top-0 transition-transform -translate-y-full sm:translate-y-0 ">
+            <Mobile setShow={setShow} />
+          </span>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ components/Mobile.js
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import { MdClose } from "react-icons/md";
+const Mobile = ({ setShow }) => {
+  return (
+    <div className="flex items-start justify-start w-full h-full p-2 flex-col">
+      <span className=" text-white w-full flex items-center justify-end p-2">
+        <MdClose onClick={() => setShow(false)} fontSize={20} />
+      </span>
+      <div className="w-full  h-3/4 flex items-center justify-evenly flex-col text-white">
+        <ul>
+          <li className="block px-2 text-xl font-bold tracking-widest my-3">
+            <Link href="/" onClick={() => setShow(false)}>
+              Home
+            </Link>
+          </li>
+          <li className="block px-2 text-xl font-bold tracking-widest my-3">
+            <Link href="/about" onClick={() => setShow(false)}>
+              Blog
+            </Link>
+          </li>
+          <li className="block px-2 text-xl font-bold tracking-widest my-3">
+            <Link href="/contact" onClick={() => setShow(false)}>
+              About
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="w-full h-1/4  text-white flex items-center justify-center">
+        <ul>
+          <li className="inline-block px-2 text-sm font-semibold mx-2">
+            <Link href="/login" onClick={() => setShow(false)}>
+              Login
+            </Link>
+          </li>
+          <li className="inline-block bg-black text-white p-2 rounded-md shadow text-sm font-semibold">
+            <Link href="/register" onClick={() => setShow(false)}>
+              Sign up
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Mobile;
