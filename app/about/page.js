0147f2b879e12cbcb11efc295dd9b5179d80f3ce
Ritesh <codeshorts007@gmail.com>
2023-12-04 13:38:27 +0530

    style- about apge

--- app/about/page.js
+++ app/about/page.js
@@ -1,5 +1,153 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const page = () => {
-  return <div>about page</div>;
+  return (
+    <div className="flex items-center justify-center flex-col lg:w-11/12 mx-auto mt-4">
+      <h1 className="text-4xl font-bold text-center text-gray-700">About Us</h1>
+      <span className="w-full overflow-hidden rounded-md my-2 lg:h-96">
+        <Image
+          src={"/images/about.jpg"}
+          width={1780}
+          height={200}
+          alt="about"
+          className="contrast-50 brightness-75  hue-rotate-30"
+        />
+      </span>
+      <span className="w-full flex items-center justify-center flex-col md:flex-row ">
+        <span className="md:w-1/2 flex items-start justify-start flex-col p-2 font-semibold text-[15px] ">
+          <h4 className="font-bold">{"About "}</h4>
+          Welcome to Blog - your one-stop destination for reading and writing
+          captivating blogs. As a leading blog website, we strive to provide an
+          immersive platform where individuals can freely express their
+          thoughts, share valuable insights, and connect with a diverse
+          community of like-minded individuals.
+        </span>
+        <span className="md:w-1/2 flex items-start justify-start flex-col p-2 font-semibold text-[15px] ">
+          <h4 className="font-bold">{"Company's Journey"}</h4>
+          <p>
+            Our journey began with a simple vision - to create a space where
+            individuals could unleash their creativity and thoughts through the
+            power of blogging. Since our inception, we have been committed to
+            fostering a vibrant and supportive environment for writers and
+            readers alike. With each passing year, we have grown and evolved,
+            constantly refining our platform to ensure a seamless user
+            experience.
+          </p>
+        </span>
+      </span>
+      <span className="w-full flex items-center justify-center flex-col md:flex-row ">
+        <span className="md:w-1/2 flex items-start justify-start flex-col p-2 font-semibold text-[15px] ">
+          <h4 className="font-bold">{"Purpose and Goals "}</h4>
+          At Blog, we firmly believe in the transformative power of words. Our
+          purpose is to empower voices worldwide by providing a platform that
+          enables writers to communicate their ideas, engage with others, and
+          make a lasting impact. We strive to create an inclusive space, where
+          diverse perspectives merge, fostering insightful discussions and
+          encouraging personal growth.
+          <br />
+          <br />
+          Our goals revolve around fostering creativity, encouraging
+          knowledge-sharing, and nurturing a passionate and engaged community.
+          By curating compelling content and facilitating meaningful
+          interactions, we aim to inspire, educate, and entertain our audience.
+        </span>
+        <span className="md:w-1/2 flex items-start justify-start flex-col p-2 font-semibold text-[15px] ">
+          <h4 className="font-bold">{"Introduction to the Team"}</h4>
+          <p>
+            Behind the scenes, our dedicated team of skilled professionals is
+            the backbone of Blog. We are a diverse group of writers, editors,
+            developers, and enthusiasts united by our love for literary
+            expression. With years of collective experience and a deep
+            understanding of the blogging landscape, our team works tirelessly
+            to ensure every visitors journey on our platform is enriching and
+            enjoyable.
+          </p>
+        </span>
+      </span>
+      <span className="w-full overflow-hidden rounded-md my-2 lg:h-96">
+        <Image
+          src={"/images/about2.jpg"}
+          width={1780}
+          height={200}
+          alt="about"
+          className="contrast-50 brightness-75  hue-rotate-30"
+        />
+      </span>
+      <span className="w-full flex items-center justify-center flex-col md:flex-row ">
+        <span className="md:w-1/2 flex items-start justify-start flex-col p-2 font-semibold text-[15px] ">
+          <h4 className="font-bold">{"Offerings "}</h4>
+          At Blog, we strive to provide an extensive array of offerings designed
+          to enhance the blogging experience. We offer user-friendly tools and
+          features, enabling writers to unleash their creativity effortlessly.
+          From customizable templates to seamless publishing and sharing
+          options, our platform aims to empower bloggers and amplify their
+          reach.
+          <br />
+          <br />
+          Additionally, we curate a wide range of topics to cater to every
+          interest and passion, allowing readers to explore and discover
+          captivating content. We welcome bloggers from all walks of life,
+          providing them with a platform to showcase their unique perspectives
+          and connect with a like-minded audience.
+        </span>
+        <span className="md:w-1/2 flex items-start justify-start flex-col p-2 font-semibold text-[15px] ">
+          <h4 className="font-bold">{"Customer Opinions"}</h4>
+          <p>
+            The satisfaction and feedback of our valued users have always been
+            the driving force behind our constant improvement. Heres what a few
+            of our cherished community members have to say:
+            <br />
+            <br />
+            {
+              "1. Blog has not only revolutionized the way I express myself but also connected me with an incredible community of passionate writers. '- Sarah, avid blogger.'"
+            }
+            <br />
+            {
+              "2. I have been a part of Blog for years, and the support and encouragement I have received from fellow bloggers have been invaluable. It truly feels like a second home.' - Kevin, dedicated writer.'"
+            }
+          </p>
+        </span>
+      </span>
+      <span className="w-full flex items-center justify-center flex-col md:flex-row ">
+        <span className="md:w-1/2 flex items-start justify-start flex-col p-2 font-semibold text-[15px] ">
+          <h4 className="font-bold">{"Achievements"}</h4>
+          Over the years, Blog has achieved significant milestones, solidifying
+          our position as a leading blog website. We have consistently garnered
+          recognition for our innovative approach and commitment to user
+          satisfaction. Some of our notable achievements include:
+          <br />
+          <br />
+          {"1. Trusted platform for millions of writers and readers worldwide."}
+          <br />
+          {
+            "2. Tens of thousands of engaging and thought-provoking blogs published."
+          }
+          <br />
+          {
+            "3. Regularly featured in prominent publications and media outlets for our industry-leading contributions."
+          }
+        </span>
+        <span className="md:w-1/2 flex items-start justify-start flex-col p-2 font-semibold text-[15px] ">
+          <h4 className="font-bold">{"Call to Action"}</h4>
+          <p>
+            Join us today and become a part of our dynamic community. Whether
+            youre an aspiring writer searching for a platform to unleash your
+            creativity or an avid reader seeking captivating content, Blog is
+            here to fulfill your blogging needs. Embrace the power of words and
+            embark on an enriching journey with us
+            <br />
+            Visit{" "}
+            <Link href="/" className="text-blue-600">
+              {" "}
+              Blog{" "}
+            </Link>{" "}
+            now and start exploring, reading, and writing captivating blogs!
+          </p>
+        </span>
+      </span>
+    </div>
+  );
 };
 
 export default page;
