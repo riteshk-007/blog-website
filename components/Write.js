370356b147cd1757adb8692699c5c59df3a0ccc3
Ritesh <codeshorts007@gmail.com>
2023-12-08 14:36:33 +0530

    fix- react-quill error

--- components/Write.js
+++ components/Write.js
@@ -2,46 +2,48 @@
 
 import { PostContext } from "@/Context/PostApi";
 import { useContext } from "react";
-import ReactQuill from "react-quill";
 import "react-quill/dist/quill.bubble.css";
 import dynamic from "next/dynamic";
 
 const Write = () => {
   const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
+
   const { createPost, value, setValue, posts, setPosts } =
     useContext(PostContext);
+
   const handleSubmit = () => {
     createPost();
   };
+
   return (
     <div className="flex items-center relative p-5 flex-col">
       <button
         onClick={handleSubmit}
-        className="absolute lg:right-4 right-2 lg:top-4 top-2   px-4 py-1 bg-green-800 hover:bg-green-900 rounded-md shadow-md font-semibold text-white"
+        className="absolute lg:right-4 right-2 lg:top-4 top-2 px-4 py-1 bg-green-800 hover:bg-green-900 rounded-md shadow-md font-semibold text-white"
       >
         publish
       </button>
-      <div className="my-10  lg:w-4/5 mx-auto">
+      <div className="my-10 lg:w-4/5 mx-auto">
         <input
           type="text"
           placeholder="Title..."
-          className="w-full px-2 py-1  border-none outline-none font-semibold text-4xl"
+          className="w-full px-2 py-1 border-none outline-none font-semibold text-4xl"
           value={posts}
           onChange={(e) => setPosts(e.target.value)}
           required
         />
       </div>
       <div className="w-full lg:w-4/5 mx-auto my-2">
-        <ReactQuill
-          theme="bubble"
-          value={value}
-          onChange={setValue}
-          placeholder="Tell your story..."
-          required
-        />
+        {typeof document !== "undefined" && (
+          <ReactQuill
+            theme="bubble"
+            value={value}
+            onChange={setValue}
+            placeholder="Tell your story..."
+          />
+        )}
       </div>
     </div>
   );
 };
-
 export default Write;
